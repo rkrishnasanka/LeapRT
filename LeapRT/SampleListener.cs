a72f755e667d49dc5f07f3d9ad8ea19fd0feacbc
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2013-04-20 22:50:40 +0530

    Removed some default code given by leap

--- LeapRT/SampleListener.cs
+++ LeapRT/SampleListener.cs
@@ -155,24 +155,3 @@ class SampleListener : Listener
 		}
 	}
 }
-
-//class Sample
-//{
-//    public static void Main ()
-//    {
-//        // Create a sample listener and controller
-//        SampleListener listener = new SampleListener ();
-//        Controller controller = new Controller ();
-
-//        // Have the sample listener receive events from the controller
-//        controller.AddListener (listener);
-
-//        // Keep this process running until Enter is pressed
-//        Console.WriteLine ("Press Enter to quit...");
-//        Console.ReadLine ();
-
-//        // Remove the sample listener when done
-//        controller.RemoveListener (listener);
-//        controller.Dispose ();
-//    }
-//}
